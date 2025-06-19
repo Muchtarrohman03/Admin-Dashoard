330170bce44d605c25863386d4f75c3a2fcf8907
Muchtarrohman03 <muchtarrohman37@gmail.com>
2025-06-19 19:46:05 +0700

    perbaikan navigasi ukuran ayar kecil dan edit footer

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -117,18 +117,11 @@
     @click.outside="open = false"
     >
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            @foreach ($menus as $menu)
+                <x-responsive-nav-link :href="route($menu['route'])" :active="request()->routeIs($menu['route'])">
+                {{ __($menu['label']) }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('product.index')" :active="request()->routeIs('product.index')">
-                {{ __('Product') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('services.index')" :active="request()->routeIs('services.index')">
-                {{ __('Service') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('orders.index')" :active="request()->routeIs('orders.index')">
-                {{ __('Pesanan') }}
-            </x-responsive-nav-link>
+            @endforeach
         </div>
 
         <!-- Responsive Settings Options -->
